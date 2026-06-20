62cd7ee7fc42dffecea119fb3ae699ad7c31899f
State Capture <statecapture2026@gmail.com>
2026-06-20 20:01:14 +0200

    Update TARIFF_BASE_URL for testing and README for new downloadable tariff schema

--- app/src/main/java/za/co/statecapture/android/util/AppConstants.kt
+++ app/src/main/java/za/co/statecapture/android/util/AppConstants.kt
@@ -44,5 +44,5 @@ object AppConstants {
     const val ADMOB_BANNER_UNIT_ID = "ca-app-pub-5657991828045333/5594828795" // Unit: Bottom Banner
     const val ADMOB_NATIVE_UNIT_ID = "ca-app-pub-5657991828045333/7279061111" // Unit: Support
 
-    const val TARIFF_BASE_URL = "https://raw.githubusercontent.com/StateCapture/StateCapture/main/tariffs/"
+    const val TARIFF_BASE_URL = "https://raw.githubusercontent.com/StateCapture/StateCapture/feat/downloadable-tariffs/tariffs/"
 }
