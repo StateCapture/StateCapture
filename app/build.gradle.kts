515a00e56b57c96ff2ebf6beca0885b35a2d35b7
State Capture <statecapture2026@gmail.com>
2026-08-11 09:50:10 +0200

    build: update androidx.fragment dependency to 1.8.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
