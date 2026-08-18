9b4c2a209dd116d531919e5ca299e9cae66ce4ec
State Capture <statecapture2026@gmail.com>
2026-08-18 11:55:45 +0200

    chore: bump version to 1.4.1 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "za.co.statecapture.android"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.4.0"
+        versionCode = 10
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
